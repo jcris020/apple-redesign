7bbb38cb18fb245c910e19161d0ec44651304073
Jesse Cristobal <jcris020@fiu.edu>
2022-11-09 17:26:37 -0500

    resolved session type error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { store } from "../redux/store";
 import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps }, }: AppProps) {
   return (
     <SessionProvider session={session}>
       <Provider store={store}>
